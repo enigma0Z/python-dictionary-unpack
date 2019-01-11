05fda4af4a2218dc88502caa55f1cd8e3f5b1086
Johnathan Bell <enigma.0ZA@gmail.com>
2019-01-11 18:20:25 -0500

    Add docs / doc config

--- Unpack/Unpack.py
+++ Unpack/Unpack.py
@@ -1,29 +1,49 @@
+"""
+Usage::
+
+    from unpack import Unpack
+"""
 class Unpack:
+    """
+    * Unpack a dictionary into a class with the dictionary's keys as members.
+    * Unpack a list into a class with `.items` as a member that contains the list's members
+
+    .. testsetup:: *
+
+        from unpack import Unpack
+
+    .. doctest::
+
+        >>> v = Unpack({"a": "one", "b": "two"})
+
+        >>> v.a
+        'one'
+    """
     def __init__(self, data):
-        unpacked_data = self._unpack(data)
-        if type(unpacked_data) == dict:
-            self.__dict__.update(unpacked_data)
-        elif type(unpacked_data) == list:
-            self.__dict__.update({'items': unpacked_data})
+        unpackedData = self._unpack(data)
+        if isinstance(unpackedData, dict):
+            self.__dict__.update(unpackedData)
+        elif isinstance(unpackedData, list):
+            self.__dict__.update({'items': unpackedData})
 
     def __repr__(self):
         return "Unpack({contents})".format(contents=self.__dict__)
 
     def _create(self, data):
-        if type(data) == dict:
+        if isinstance(data, dict):
             return Unpack(data)
-        elif type(data) == list:
-            if data != [] and type(data[0]) == dict:
+        elif isinstance(data, list):
+            if data != [] and isinstance(data[0], dict):
                 return list(map(lambda x: Unpack(x), data))
 
         return data
 
     def _unpack(self, data):
-        if type(data) == dict:
+        if isinstance(data, dict):
             for key in data:
                 data[key] = self._create(data[key])
 
-        elif type(data) == list:
+        elif isinstance(data, list):
             data = self._create(data)
 
         return data
--- Unpack/__init__.py
+++ Unpack/__init__.py
@@ -1,1 +1,63 @@
-from .Unpack import Unpack
+"""
+Unpack Module
+-------------
+
+Getting Started
+***************
+
+Import the Unpack class::
+
+    from unpack import Unpack
+
+And then throw a dictionary into it::
+
+    unpacked_data = Unpack(data)
+
+Full Example
+************
+
+.. testsetup:: *
+
+    from unpack import Unpack
+
+Code:
+
+.. testcode::
+
+    from unpack import Unpack
+
+    my_data = {
+        "attribute": "value",
+        "dictMember": {
+            "blue": "0x0000ff",
+            "red": "0xff0000"
+        },
+        "listMember": [ "one", "two", "three" ],
+        "listOfDicts": [
+            {
+                "a": 1,
+                "b": 2
+            }, {
+                "c": 3,
+                "d": 4
+            }
+        ]
+    }
+
+    my_unpacked_data = Unpack(my_data)
+
+    print(my_unpacked_data.attribute)
+    print(my_unpacked_data.dictMember.blue)
+    print(my_unpacked_data.listMember[0])
+    print(my_unpacked_data.listOfDicts[0].a)
+
+Expected Output:
+
+.. testoutput::
+
+    value
+    0x0000ff
+    one
+    1
+"""
+from .unpack import Unpack
